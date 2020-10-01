bb8812c8fc54612c7bee37360ad1e7d68c103dd5
Terver Ugbe <32528384+Dennisruso@users.noreply.github.com>
2020-10-01 17:40:49 +0100

    Script.js
    script page linked

--- index.js
+++ index.js
@@ -26,3 +26,23 @@ var MenuItems = document.getElementById("MenuItems");
         RegForm.style.transform = "translateX(300px)";
         LoginForm.style.transform = "translateX(300px)";
     }
+    /*------------------------Products gallery-------------------------------------*/
+    var ProductImg = document.getElementById("ProductImg");
+    var SmallImg = document.getElementsByClassName("small-img");
+
+    SmallImg[0].onclick = function()
+    {
+        ProductImg.src = SmallImg[0].src;
+    }
+    SmallImg[1].onclick = function()
+    {
+        ProductImg.src = SmallImg[1].src;
+    }
+    SmallImg[2].onclick = function()
+    {
+        ProductImg.src = SmallImg[2].src;
+    }
+    SmallImg[3].onclick = function()
+    {
+        ProductImg.src = SmallImg[3].src;
+    }
